b866dbd33cd9fa1f037b01aeac8892ab31d0b5b5
Jared Avila <jared.c.avila@gmail.com>
2019-01-17 11:56:47 -0800

    basic recipe search app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+import { ResultsComponent } from "./results/results.component";
+import { SearchComponent } from "./search/search.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: SearchComponent
+  },
+  {
+    path: "results",
+    component: ResultsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CarouselModule } from "ngx-bootstrap";
+import { CarouselComponent } from './carousel/carousel.component';
+import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CarouselComponent, SearchComponent, ResultsComponent],
+  imports: [BrowserModule, AppRoutingModule, CarouselModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
